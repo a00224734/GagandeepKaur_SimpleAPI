d5a0187d4011201e2f9f0861e777d4bcc39d2ae4
Gagandeep Kaur <a00224734@mycambrian.ca>
2020-12-01 14:41:19 +0530

    SimpleAPI

---
+++ GagandeepKaur_A00224734_SimpleAPI/Controllers/RandomController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GagandeepKaur_A00224734_SimpleAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        Random random = new Random();
+        [HttpGet]
+        public int Get()
+        {
+            double flt = random.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            return shift;
+        }
+
+        [HttpPost]
+        public double Post()
+        {
+            double flt = random.NextDouble();
+            return flt;
+        }
+
+        [HttpPut]
+        public String Put()
+        {
+            return "Thank you for the new number!";
+        }
+
+        [HttpDelete]
+        public String Delete()
+        {
+            double flt = random.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            char letter = Convert.ToChar(shift + 65);
+            return letter.ToString();
+        }
+
+        [HttpPatch]
+        public String Patch()
+        {
+            DateTime date = DateTime.Now;
+            return date.ToString();
+        }
+
+        [HttpOptions]
+        public String Options()
+        {
+            var option = "GET: Return a Random Integer.\n" + "POST: Return a Random Double.\n" + "PUT: Return a String.\n" + "DELETE: Return a Random Character.\n" + "PATCH: Gives the Current Date.\n" + "OPTIONS: Return Some Text About What Each Method Returned.";
+            return option;
+        }
+    }
+}
---
+++ GagandeepKaur_A00224734_SimpleAPI/Models/Random.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GagandeepKaur_A00224734_SimpleAPI.Models
+{
+    public class Random
+    {
+        public Double Flt { get; set; }
+        public int Shift { get; set; }
+        public char Letter { get; set; }
+        public DateTime Date { get; set; }
+        public string Option { get; set; }
+    }
+}
